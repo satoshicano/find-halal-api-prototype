7275665cfe13908ed669bf69e7d1fcbfb8a2a00b
satoshicano <satoshicano@gmail.com>
2017-05-09 02:03:16 +0900

    move methods to lib/vision.rb

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -12,22 +12,6 @@ class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   def check_halal
-    desc = Vision.new(image.path).request
-    { status: fetch_halal_status(desc), descriptions: desc }
-  end
-
-  # Vision.translate_annotate(halal_word)
-  def fetch_halal_status(desc)
-    halal_words = Word.pluck(:name)
-    halal_words.each do |halal_word|
-      desc.each do |d|
-        # return false if d.downcase.include?(halal_word)
-        if d.downcase.include?(halal_word)
-          Vision.new(image.path).translate_annotate(halal_word)
-          return false
-        end
-      end
-    end
-    true
+    Vision.new(image.path).check
   end
 end
--- lib/vision.rb
+++ lib/vision.rb
@@ -9,13 +9,14 @@ class Vision
     @file_path = file_path
   end
 
-  def request
+  def check
     http_client = HTTPClient.new
     content = Base64.strict_encode64(File.new(file_path, 'rb').read)
-    response = http_client.post_content(
-      endpoint, request_json(content), 'Content-Type' => 'application/json'
-    )
-    fetch_descriptions(response)
+    response = http_client.post_content(endpoint, request_json(content), 'Content-Type' => 'application/json')
+    no_halal_foods = fetch_no_halal_food(response)
+    return { status: 'halal' } if no_halal_foods.empty?
+    annotate_no_halal_food(no_halal_foods)
+    { status: 'no halal' }
   end
 
   def request_json(content)
@@ -30,15 +31,57 @@ class Vision
     }.to_json
   end
 
-  def fetch_descriptions(response)
+  def fetch_no_halal_food(response) # rubocop:disable Metrics/MethodLength
     result = JSON.parse(response)['responses'].first
-    result['textAnnotations'].map { |text| text['description'] }
+    no_halal_foods = []
+    no_halal_words = Word.pluck(:name, :english).each
+    result['textAnnotations'].map do |text|
+      no_halal_words.each do |word|
+        if text['description'].include?(word[0])
+          no_halal_foods.push(text)
+          no_halal_foods.last[:word] = word[1]
+          break
+        end
+      end
+    end
+    no_halal_foods.shift
+    no_halal_foods
   end
 
-  def translate_annotate(word)
+  def annotate_no_halal_food(no_halal_foods)
     image = Magick::ImageList.new(@file_path)
-    draw = Magick::Draw.new
-    draw.annotate(image, 100, 200, 300, 400, word)
+
+    no_halal_foods.each do |foods|
+      draw_annotate(image, calc_length(foods['boundingPoly']['vertices']), foods[:word])
+    end
+
     image.write(@file_path)
+  end
+
+  def draw_annotate(image, vertices, word)
+    draw = Magick::Draw.new
+    draw.fill('#FFFFFF')
+    draw.rectangle(vertices[:x1], vertices[:y1], vertices[:x2], vertices[:y2])
+    draw.draw(image)
+
+    draw = Magick::Draw.new
+    font = 'DejaVu-Sans'
+    draw.annotate(image, vertices[:width], vertices[:height], vertices[:x3], vertices[:y3], word) do
+      self.font = font
+      self.pointsize = 64
+    end
+  end
+
+  def calc_length(vertices)
+    {
+      x1: vertices[0]['x'],
+      y1: vertices[0]['y'],
+      x2: vertices[2]['x'],
+      y2: vertices[2]['y'],
+      x3: vertices[3]['x'],
+      y3: vertices[3]['y'],
+      height: vertices[3]['y'] - vertices[0]['y'],
+      width: vertices[1]['x'] - vertices[0]['x']
+    }
   end
 end
